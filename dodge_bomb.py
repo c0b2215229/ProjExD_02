aee86a0c0b90deb6dea501792274cfb65844fcfa
C0B22152 <c0b2215229@edu.teu.ac.jp>
2023-04-25 15:41:04 +0900

    練習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,6 +3,27 @@ import sys
 
 import pygame as pg
 
+delta = { # 練習4
+        pg.K_UP: (0, -1),
+        pg.K_DOWN: (0, +1),
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (+1, 0)
+    }
+
+def check_bound(scr_rct: pg.Rect,obj_rct:pg.Rect) -> tuple[bool, bool]: # 練習5
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数1：画面SurfaceのRect
+    引数2：こうかとん、または、爆弾Surface
+    戻り値：横方向、縦方向のはみだし判定結果（画面内：True、画面概：False)
+    """
+    yoko, tate = True, True # 練習5
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: # 練習5
+        yoko = False # 練習5
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: # 練習5
+        tate = False # 練習5
+    return yoko, tate # 練習5
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -22,12 +43,7 @@ def main():
     vx, vy = +1, +1 # 練習3
     bb_rct = bb_img.get_rect() # 練習3
     bb_rct.center = x, y # 練習3
-    delta = { # 練習4
-        pg.K_UP: (0, -1),
-        pg.K_DOWN: (0, +1),
-        pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1, 0)
-    }
+    
     tmr = 0
 
     while True:
@@ -40,17 +56,25 @@ def main():
         for k, mv in delta.items(): # 練習4
             if key_lst[k]: # 練習4
                 kk_rct.move_ip(mv) # 練習4
+        if check_bound(screen.get_rect(),kk_rct) != (True, True):
+            for k, mv in delta.items(): # 練習5
+                if key_lst[k]: # 練習5
+                    kk_rct.move_ip(-mv[0],-mv[1]) # 練習5
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct) # 練習4
         bb_rct.move_ip(vx, vy) # 練習3
+        yoko, tate = check_bound(screen.get_rect(),bb_rct) # 練習5
+        if not yoko: # 横方向にはみ出ていたら　# 練習5
+            vx *= -1 # 練習5
+        if not tate: # 縦方向にはみ出ていたら　# 練習5
+            vy *= -1 # 練習5
         screen.blit(bb_img, bb_rct) # 練習3
+        
 
         pg.display.update()
         clock.tick(1000)
 
-def new_func():
-    pg.K_LEFT: (-1, 0)
 
 
 if __name__ == "__main__":
